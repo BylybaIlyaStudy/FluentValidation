50a6a4bde10745031276097fee2c9acc6b1d73f7
BylybaIlyaStudy <73159350+BylybaIlyaStudy@users.noreply.github.com>
2021-04-19 20:45:56 +0700

    Add files via upload

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.People;
+using WebApplication1.Validators;
+
+namespace WebApplication1.Controllers
+{
+    [Controller]
+    public class PersonController : Controller
+    {
+        [HttpPost]
+        public ActionResult Post([FromBody] PersonDTO personDTO)
+        {
+            Person person = personDTO.Adapt<Person>();
+
+            var validator = new PersonValidator();
+            var results = validator.Validate(person);
+
+            if (results.IsValid)
+            {
+                return Ok();
+            }
+
+            return StatusCode(400);
+        }
+    }
+}
